a87e5f04a0b401b36ca2c89c293ff02747a130a9
JATAYLORNC <disneyjeff@gmail.com>
2018-03-06 21:30:51 -0500

    Google Places Ajax Query

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,52 @@
+var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+raleigh&key= AIzaSyB2Ys8ExJDWr3CF94ia0_Oyxm8gBM87udY&type=restaurant";
+var queryURLB = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+raleigh&key= AIzaSyB2Ys8ExJDWr3CF94ia0_Oyxm8gBM87udY&type=bar";
+var myRestaurantQuery;
+var restaurantNames = [];
+var myBarQuery;
+var barNames = [];
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+console.log(response);
+
+myRestaurantQuery = response.results;
+
+console.log(myRestaurantQuery);
+
+
+for (i=0; i<20; i++) {
+    var restname = myRestaurantQuery[i].name;
+    restaurantNames.push(restname);
+ }
+
+console.log(restaurantNames);
+
+
+});
+
+$.ajax({
+    url: queryURLB,
+    method: "GET"
+}).then(function(response) {
+console.log(response);
+
+myBarQuery = response.results;
+
+console.log(myRestaurantQuery);
+
+
+for (i=0; i<20; i++) {
+    var barname = myRestaurantQuery[i].name;
+    barNames.push(restname);
+ }
+
+console.log(barNames);
+
+
+});
+
+
+
+
