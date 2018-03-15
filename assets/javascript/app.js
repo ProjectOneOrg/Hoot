08f192a375885d4bdb17df41b3695a64bc7481b4
JATAYLORNC <disneyjeff@gmail.com>
2018-03-15 17:26:12 -0400

    fixed issue with image transparency

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -477,7 +477,7 @@ function displayPlaces(placeData) {
 
         //creating div w/place photo//
         if (placeData[i].photo==="Photo Unavailable") {
-            placeTitleDiv.append("<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAAALCAYAAAA6JMKEAAAAMElEQVR42u3TMQEAAAgDIE1u9Pl6GAE60JVMAa8WBAQBQUAQEAQEAUFAEBAEBAGOBZvoG2v/a2D9AAAAAElFTkSuQmCC'>");
+            placeTitleDiv.append("<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAABwCAQAAADxyPcNAAAAr0lEQVR42u3RMQEAAACCMO1f2hoeIwJrdFUtACIgQAQEiIAAERAgAiIgQAQEiIAAERAgAiIgQAQEiIAAERAgAiIgQAQEiIAAERAgAiIgQAQEiIAAERAgAiIgQAQEiIAAERAgAiIgQAQEiIAAERAgAiIgQAQEiIAAERAgAiIgQAQEiIAAERAgAiIgQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEQAQEiIAAERAgAgJEHw1p8QBxL7nm/AAAAABJRU5ErkJggg'>");
         } else {
             var photoURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&photoreference=" + placeData[i].photo + 
             "&key=AIzaSyB2Ys8ExJDWr3CF94ia0_Oyxm8gBM87udY";
